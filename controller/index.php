aab174772e736ca4249e11070038ba6e5b6d5b65
guaishushu <guaishushu@msn.com>
2012-04-26 06:13:15 +0800

    d

--- controller/index.php
+++ controller/index.php
@@ -1,10 +1,12 @@
 <?php  
 class Action extends SiteAction {
     public function index() {
-    	$config = _model('config')->read(array('name'=>'首页专题'));
+    	$config = _model('config')->read(array('name'=>'index'));
     	$info = _model('topic')->read(array('tid'=>$config['data']));
+    	$tags = _model('tag')->getList();
         $this->display("index.html",array(
         	'info' => $info, 
+        	'tags' => $tags,
         ));
     }
 }

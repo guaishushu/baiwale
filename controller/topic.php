550ac3beeab4656393ae4df53e8ad4e95d5d1672
guaishushu <guaishushu@msn.com>
2012-04-26 21:49:28 +0800

    d

--- controller/topic.php
+++ controller/topic.php
@@ -1,7 +1,7 @@
 <?php  
 class Action extends SiteAction {
     public function index() {
-        $info = _model('')->getList('SELECT user.uid, user.username, tag.id, tag.tag, topic.title, topic.tid, topic.retime FROM topic, user, tag WHERE topic.uid = user.uid AND topic.tag = tag.id');
+        $info = _model('')->getList('SELECT user.uid, user.username, tag.id, tag.tag, topic.title, topic.tid, topic.retime FROM topic, user, tag WHERE topic.uid = user.uid AND topic.tag = tag.id ORDER BY retime DESC');
         //$this->pvar($info);die;
         foreach ($info as $key => $value) {
             $replay = _model('replay')->read('WHERE tid = ' . $value['tid'] . ' ORDER BY addtime DESC');
@@ -58,7 +58,7 @@ class Action extends SiteAction {
     }
 
     public function tag($id) {
-        $info = _model('')->getList('SELECT user.username, tag.id, tag.tag, topic.title, topic.tid, topic.retime FROM topic, user, tag WHERE topic.uid = user.uid AND topic.tag = tag.id AND topic.tag = ' . $id);
+        $info = _model('')->getList('SELECT user.username, tag.id, tag.tag, topic.title, topic.tid, topic.retime FROM topic, user, tag WHERE topic.uid = user.uid AND topic.tag = tag.id AND topic.tag = ' . $id . ' ORDER BY retime DESC');
         //$this->pvar($info);die;
         foreach ($info as $key => $value) {
             $replay = _model('replay')->read('WHERE tid = ' . $value['tid'] . ' ORDER BY addtime DESC');
@@ -149,6 +149,7 @@ class Action extends SiteAction {
         $info['tid'] = $tid;
         $info['addtime'] = time();
         $id = _model('replay')->create($info);
+        _model('topic')->update(array('tid'=>$tid), array('retime'=>$info['addtime']));
         $this->redirect("topic/view/$tid.html");
     }
 }

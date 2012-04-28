561a15361756f4dfa3f2561e65e0a84747b33042
guaishushu <guaishushu@msn.com>
2012-04-28 09:11:49 +0800

    d

--- controller/about.php
+++ controller/about.php
@@ -1,8 +1,10 @@
 <?php  
 class Action extends SiteAction {
     public function index() {
+    	$config = _model('config')->read(array('name'=>'about'));
         $this->display("about.html", array(
         	'active' => 'about',
+        	'config' => $config,
         ));
     }
 }
--- controller/contact.php
+++ controller/contact.php
@@ -1,8 +1,10 @@
 <?php  
 class Action extends SiteAction {
     public function index() {
+    	$config = _model('config')->read(array('name'=>'contact'));
         $this->display("contact.html", array(
         	'active' => 'contact',
+        	'config' => $config,
         ));
     }
 }
